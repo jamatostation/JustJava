c3fd89770012bc61997d626f2b588128c6423b5e
jamatostation <jmamato1990@gmail.com>
2020-03-22 13:42:39 -0400

    Change: layout for have horizontal plus and minus. Add strings to the variable to shows up when the order button is pressed.

--- app/src/main/java/com/example/justjava/MainActivity.java
+++ app/src/main/java/com/example/justjava/MainActivity.java
@@ -33,7 +33,10 @@ public class MainActivity extends AppCompatActivity {
      * This method is called when the order button is clicked.
      */
     public void submitOrder(View view) {
-        displayPrice(quantity*5);
+        String priceMessage = "";
+        priceMessage = "Total: $" + quantity * 5;
+        priceMessage = priceMessage + "\nThank You!";
+        displayMessage(priceMessage);
     }
 
     /**
@@ -50,6 +53,14 @@ public class MainActivity extends AppCompatActivity {
     private void displayPrice(int number) {
         TextView priceTextView = (TextView) findViewById(R.id.price_text_view);
         priceTextView.setText(NumberFormat.getCurrencyInstance().format(number));
+    }
+
+    /**
+     * This method displays the given text on the screen.
+     */
+    private void displayMessage(String message) {
+        TextView priceTextView = (TextView) findViewById(R.id.price_text_view);
+        priceTextView.setText(message);
     }
 
     /**
